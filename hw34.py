884d07f0a51665400b4fa1ad8013d3c52819e86d
PetrKavan31 <petrkavan31@gmail.com>
2024-05-05 15:54:20 +0200

    add hw34 file, class + def methods, def main(), run

--- hw34.py
+++ hw34.py
@@ -30,26 +30,32 @@ class CountryCapitalDict:
     def save_data(self, filename):
         with open(filename, "wb") as file:
             pickle.dump(self.data, file)
+            print("Data saved successfully.")
 
     def load_data(self, filename):
         with open(filename, "rb") as file:
             self.data = pickle.load(file)
+            print("Data loaded successfully.")
+
+    def __str__(self):
+        return f"{self.data}"
+
 
 def main():
     ccd = CountryCapitalDict()
     ccd.add_data('Czechia', 'Prague')
     ccd.add_data('Poland', 'Warsaw')
+    print(ccd.find_data("Czechia"))
+    ccd.edit_data('Czechia', 'Ostrava')
+    print(ccd.find_data("Czechia"))
+    ccd.delete_data("Czechia")
+    print(ccd.find_data("Czechia"))
+    ccd.save_data("data.pkl")
+    ccd.load_data("data.pkl")
+    # loaded_data = ccd.load_data("data.pkl")
+    # print(f"Loaded data from file: {loaded_data}")
 
 
 if __name__ == "__main__":
     main()
 
-
-
-
-
-countries_capitals = {
-    'Czechia': 'Prague',
-    'Poland': 'Warsaw',
-    'Ukraine': 'Kyiv'
-}
